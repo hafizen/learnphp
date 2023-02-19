3591025544fddb8ef7069cbd16409c195cca7de7
Hafiz Kurniawan <99454377+hafizen@users.noreply.github.com>
2023-02-19 22:58:52 +0700

    Delete 04 directory

--- 04/401function.php
+++
@@ -1,9 +0,0 @@
-<!-- fungsi/ module 
-
-1 bulit in function
-2. user defined function
-
-fungsi berhubungan dengan date/time
-
--->
-
--- 04/402_date.php
+++
@@ -1,7 +0,0 @@
-<?php
-    echo date("l, d-M-Y");
-    echo "<br>";
-    echo date("<br>l, d M Y", time()+60*60*24*11);
-?>
-
-
